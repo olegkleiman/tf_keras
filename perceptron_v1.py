d7396d410e8b25c8663f7b09d69a7946b9201b9e
Oleg Kleiman <oleg_kleyman@hotmail.com>
2019-10-15 01:38:53 +0300

    Simplest perceptron

---
+++ perceptron_v1.py
@@ -0,0 +1,68 @@
+import tensorflow as tf
+
+# W = tf.Variable(tf.ones(shape=(2,2)), name="W")
+# b = tf.Variable(tf.zeros(shape=(2)), name="b")
+
+# @tf.function
+# def forward(x):
+#   print(W)    
+#   return W * x + b
+
+# out_a = forward([1,0])
+# print(out_a)  
+
+# out_b = forward([0,1])
+# regularizer = tf.keras.regularizers.l2(0.04)
+# reg_loss = regularizer(W)
+
+# Enabling eager execution changes how TensorFlow operations behave.
+# now they immediately evaluate and return their values to Python
+
+# # In Tensorflow 2.0, eager execution is enabled by default.
+# tf.executing_eagerly()
+tf.compat.v1.disable_eager_execution()
+
+print("Devices: ", len(tf.config.experimental.list_physical_devices(device_type=None)))
+# tf.debugging.set_log_device_placement(True)
+
+x = tf.constant(1.0, name='input')
+w = tf.Variable(0.8, name='weight')
+y = tf.multiply(w, x, name='output')
+
+y_ = tf.constant(0.0)
+loss = (y - y_)**2
+# @tf.function
+# def loss(y):
+#     return (y - y_)**2
+
+optim =  tf.compat.v1.train.GradientDescentOptimizer(learning_rate = 0.025)
+
+# Stochastic gradient descent
+# optim = tf.keras.optimizers.SGD(learning_rate = 0.025)
+# step = optim.minimize(loss, var_list = w)
+
+grads_and_vars = optim.compute_gradients(loss)
+print(grads_and_vars) # should be 1.6 after session run, 
+                      # for a meanwhile prints the reference to graph's node  
+
+# optim.apply_gradients(grads_and_vars)
+
+sess = tf.compat.v1.Session()
+sess.run(tf.compat.v1.global_variables_initializer())
+print(sess.run(grads_and_vars[0][0]))
+
+sess.run(optim.apply_gradients(grads_and_vars))
+print(sess.run(w)) # should be 0.76
+# The weight decreased by 0.04 because the optimizer subtracted 
+# the gradient times the learning rate, 1.6 * 0.025, pushing the weight in the right direction.
+
+def fit(epochs):
+    calculated_weight = 0
+    for step in range(epochs):
+        sess.run(grads_and_vars[0][0]) # calculate new gradients
+        sess.run(optim.apply_gradients(grads_and_vars))
+        calculated_weight = sess.run(w) # should decrease each spep because the same resons
+        
+    return calculated_weight    
+
+print(fit(100))
